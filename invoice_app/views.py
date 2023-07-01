b1461f7e3dff2a819e44693a3d317127b3a159b3
ayush27prasad <ayushprasadcool@gmail.com>
2023-07-01 23:39:25 +0530

    Test

--- invoice_app/views.py
+++ invoice_app/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template.loader import get_template
 from django.views import View
 from xhtml2pdf import pisa
-from .models import Invoice, Item
+from datetime import datetime
+from .utils import render_to_pdf
+
+
 
 def invoice_page(request):
     if request.method == 'POST':
         billing_address = request.POST.get('billingAddress')
         invoice_number = request.POST.get('invoiceNumber')
+        current_date = datetime.now().date()
         items = []
         i = 0
         sub_total = 0
         while(request.POST.get('item_rate[%d]'%i) != None):
             quantity = request.POST.get('item_quantity[%d]'%i)
             rate = request.POST.get('item_rate[%d]'%i)
-            total = int(quantity) * float(rate)
+            total = round(int(quantity) * float(rate),2)
             items.append({
                  'description' : request.POST.get('item_description[%d]'%i),
                  'quantity' : quantity,
@@ -24,60 +26,19 @@ def invoice_page(request):
                         })
             sub_total += total
             i+=1   
-
+        tax = round(0.15*sub_total,2)
+        net_total = sub_total + tax
         context = {
             'billing_address': billing_address,
             'invoice_number': invoice_number,
             'items': items,
-            'sub_total' : sub_total
+            'sub_total' : sub_total,
+            'tax' : tax,
+            'net_total' : net_total,
+            'current_date' : current_date
                 }
+        print(render_to_pdf('invoice.html',context))
         return render(request, 'invoice.html', context)
     else:
         return render(request, 'invoice_page.html')
 
-# def invoice(request):
-#     if request.method == 'POST':
-#         billing_address = request.POST.get('billingAddress')
-#         invoice_number = request.POST.get('invoiceNumber')
-#         item_descriptions = request.POST.getlist('item_description[]')
-#         item_quantities = request.POST.getlist('item_quantity[]')
-#         item_rates = request.POST.getlist('item_rate[]')
-
-#         items = []
-#         for description, quantity, rate in zip(item_descriptions, item_quantities, item_rates):
-#             item = {
-#                 'description': description,
-#                 'quantity': quantity,
-#                 'rate': rate
-#             }
-#             items.append(item)
-
-#         context = {
-#             'billing_address': billing_address,
-#             'invoice_number': invoice_number,
-#             'items': items
-#         }
-#         return render(request, 'invoice.html', context)
-
-#     return render(request, 'invoice.html')
-def invoice(request):
-    billing_address = request.POST.get('billingAddress')
-    invoice_number = request.POST.get('invoiceNumber')
-    item_descriptions = request.POST.getlist('item_description[]')
-    item_quantities = request.POST.getlist('item_quantity[]')
-    item_rates = request.POST.getlist('item_rate[]')
-    items = []
-    for description, quantity, rate in zip(item_descriptions, item_quantities, item_rates):
-            item = {
-                'description': description,
-                'quantity': quantity,
-                'rate': rate
-            }
-            items.append(item)
-
-    context = {
-            'billing_address': billing_address,
-            'invoice_number': invoice_number,
-            'items': items
-    }
-    return render(request, 'invoice.html', context)
--- invoice_project/urls.py
+++ invoice_project/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from django.contrib import admin
-from invoice_app.views import invoice_page, invoice
+from invoice_app.views import invoice_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('invoice_page/', invoice_page, name='invoice_page'),
-    path('invoice/', invoice, name='invoice'),
 ]
